e6b224307471b41581146ddc5b50318ca9f25953
samleemcknight <samleemcknight@gmail.com>
2021-02-10 17:22:03 -0700

    fixed problem on clue #5

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -41,7 +41,8 @@ SELECT name from country where code = 'BRA';
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+SELECT capital FROM country WHERE code = 'BRA';
+SELECT name FROM city WHERE id = 211;
 --Brasilia
 
 
